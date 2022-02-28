9452f9ae4d407dab78d3aeebb092d56d3008fdee
edutekno <54890481+edutekno@users.noreply.github.com>
2022-02-28 13:46:39 +0700

    Create tts.py

---
+++ tts.py
@@ -0,0 +1,9 @@
+textTS = "hello, welcome to the future of metaverse"
+ 
+from gtts import gTTS
+from IPython.display import Audio
+tts = gTTS(textTS)
+#tts = gTTS(textTS, lang="en-US")
+tts.save('1.wav')
+sound_file = '1.wav'
+Audio(sound_file, autoplay=True)

bbff076da3460a7548525c372731532f6a62528c
edutekno <54890481+edutekno@users.noreply.github.com>
2022-02-28 12:46:11 +0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+# Text Input
+
+# save the input text in the variable 'name'
+# first argument shows the title of the text input box
+# second argument displays a default text inside the text input area
+name = st.text_input("Enter Your name", "Type Here ...")
+
+# display the name when the submit button is clicked
+# .title() is used to get the input text string
+if(st.button('Submit')):
+	result = name.title()
+	st.success(result)
